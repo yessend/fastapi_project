442faa7ca8f5c0ab9165277d4f9a7f5679431640
yessend <esen0105@mail.ru>
2026-03-02 17:20:27 +0500

    Started learning path parameters

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ def home(request: Request):
         {"posts": posts, "title": "Home"}
     )
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
+@app.get("/api/post/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    return {"error": "Post not found..."}
